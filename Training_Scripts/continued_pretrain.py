9ebae4cde548738efc91df5268d059a605497be3
QueenieLiangrf <98098460+QueenieLiangrf@users.noreply.github.com>
2024-08-01 19:24:02 +0800

    Update continued_pretrain.py

--- Training_Scripts/continued_pretrain.py
+++ Training_Scripts/continued_pretrain.py
@@ -436,30 +436,30 @@ def main():
     #         max_eval_samples = min(len(eval_dataset), data_args.max_eval_samples)
     #         eval_dataset = eval_dataset.select(range(max_eval_samples))
 
-        def preprocess_logits_for_metrics(logits, labels):
-            if isinstance(logits, tuple):
-                # Depending on the model and config, logits may contain extra tensors,
-                # like past_key_values, but logits always come first
-                logits = logits[0]
-            return logits.argmax(dim=-1)
+    def preprocess_logits_for_metrics(logits, labels):
+        if isinstance(logits, tuple):
+            # Depending on the model and config, logits may contain extra tensors,
+            # like past_key_values, but logits always come first
+            logits = logits[0]
+        return logits.argmax(dim=-1)
 
 
-        def compute_metrics(eval_preds):
-            preds, labels = eval_preds
-            # preds have the same shape as the labels, after the argmax(-1) has been calculated
-            # by preprocess_logits_for_metrics but we need to shift the labels
-            labels = labels[:, 1:].reshape(-1)
-            preds = preds[:, :-1].reshape(-1)
-            return {
-                "accuracy": float(
-                    accuracy_score(
-                        labels, 
-                        preds, 
-                        normalize=True, 
-                        sample_weight=None
-                    )
+    def compute_metrics(eval_preds):
+        preds, labels = eval_preds
+        # preds have the same shape as the labels, after the argmax(-1) has been calculated
+        # by preprocess_logits_for_metrics but we need to shift the labels
+        labels = labels[:, 1:].reshape(-1)
+        preds = preds[:, :-1].reshape(-1)
+        return {
+            "accuracy": float(
+                accuracy_score(
+                    labels, 
+                    preds, 
+                    normalize=True, 
+                    sample_weight=None
                 )
-            }
+            )
+        }
 
     # Initialize our Trainer
     trainer = Trainer(
