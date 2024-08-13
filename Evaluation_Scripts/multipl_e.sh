fcb07c62828cbea0c7e5931699f84e9f109cdec1
QueenieLiangrf <98098460+QueenieLiangrf@users.noreply.github.com>
2024-08-13 16:43:00 +0800

    Update multipl_e.sh

--- Evaluation_Scripts/multipl_e.sh
+++ Evaluation_Scripts/multipl_e.sh
@@ -1,11 +1,9 @@
-modelname="$1"
-
 for lang in "d" "cpp" "go" "py" "rb" "rs" "swift"
 do
     for temperature in 0.2 0.8
     do
-        python /code-harness/main.py \
-            --model $modelname \
+        python /Evaluation_Scripts/code-harness/main.py \
+            --model /kaggle/input/modelgo/adapter_model.safetensors \
             --max_length_generation 1024 \
             --tasks "multiple-$lang" \
             --temperature $temperature \
@@ -14,8 +12,8 @@ do
             --allow_code_execution \
             --continuous_batching_size 32 \
             --swap_space 128 \
-            --save_references_path "/Outputs/$modelname/multipl-e/$lang/$temperature/references.json" \
-            --save_generations_path "/Outputs/$modelname/multipl-e/$lang/$temperature/generations.json" \
-            --metric_output_path "/Outputs/$modelname/multipl-e/$lang/$temperature/metrics.json" 
+            --save_references_path "/kaggle/working/$modelname/multipl-e/$lang/$temperature/references.json" \
+            --save_generations_path "/kaggle/working/$modelname/multipl-e/$lang/$temperature/generations.json" \
+            --metric_output_path "/kaggle/working/$modelname/multipl-e/$lang/$temperature/metrics.json" 
     done
 done
