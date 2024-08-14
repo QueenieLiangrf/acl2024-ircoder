edb5f8ae12b9c07a4e35ca715b61c0e4f9042c4f
QueenieLiangrf <98098460+QueenieLiangrf@users.noreply.github.com>
2024-08-14 16:02:40 +0800

    Update multipl_e.sh

--- Evaluation_Scripts/multipl_e.sh
+++ Evaluation_Scripts/multipl_e.sh
@@ -2,8 +2,8 @@ for lang in "d" "cpp" "go" "py" "rb" "rs" "swift"
 do
     for temperature in 0.2 0.8
     do
-        python /kaggle/working/ircoder97/Evaluation_Scripts/code-harness/main.py \
-            --model "/kaggle/input/modelgo/" \
+        python /kaggle/working/ircoder1/Evaluation_Scripts/code-harness/main.py \
+            --model "/kaggle/working/model-go" \
             --max_length_generation 1024 \
             --tasks "multiple-$lang" \
             --temperature $temperature \
