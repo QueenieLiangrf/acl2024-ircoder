7f7f940d00ec572c74c2283922b575e0ee9f8626
QueenieLiangrf <98098460+QueenieLiangrf@users.noreply.github.com>
2024-08-19 21:35:51 +0800

    Update continued_pretrain.py

--- Training_Scripts/continued_pretrain.py
+++ Training_Scripts/continued_pretrain.py
@@ -538,7 +538,7 @@ def main():
    
         train_result = trainer.train(resume_from_checkpoint=checkpoint)
 
-        trainer.save_model()  # Saves the tokenizer too for easy upload
+        trainer.save_model("/kaggle/working/")  # Saves the tokenizer too for easy upload
 
         from deepspeed.utils.zero_to_fp32 import get_fp32_state_dict_from_zero_checkpoint
         from peft import get_peft_model_state_dict
@@ -546,15 +546,15 @@ def main():
             print(model.state_dict().keys())
             if trainer.deepspeed and not os.path.exists("/kaggle/working/pytorch_model.bin"):
                 print("CONVERT Deepspeed Checkpoint to FP32")
-                state_dict = get_fp32_state_dict_from_zero_checkpoint("output") # already on cpu
+                state_dict = get_fp32_state_dict_from_zero_checkpoint("/kaggle/working/") # already on cpu
             else:
                 print("TRY to use the model directly")
                 state_dict = model.cpu().state_dict()
             print("Number of elements in the state dict", sum(p.numel() for p in state_dict.values()))
             d = get_peft_model_state_dict(model, state_dict=state_dict)
 
-        model.save_pretrained("/kaggle/working/")
-        torch.save(d, "/kaggle/working/adapter_model.bin")
+        model.save_pretrained("/kaggle/working/lora")
+        torch.save(d, "/kaggle/working/lora/adapter_model.bin")
 
         metrics = train_result.metrics
         # from huggingface_hub import HfApi
