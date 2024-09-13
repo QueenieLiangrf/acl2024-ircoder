5c56722ee28307b7724045059c702095e7819870
QueenieLiangrf <98098460+QueenieLiangrf@users.noreply.github.com>
2024-09-13 16:18:52 +0800

    Update multipl_e.sh

--- Evaluation_Scripts/multipl_e.sh
+++ Evaluation_Scripts/multipl_e.sh
@@ -3,14 +3,14 @@ do
     for temperature in 0.2 0.8
     do
         python /kaggle/working/ircoder1/Evaluation_Scripts/code-harness/main.py \
-            --model "/kaggle/working/model-go" \
+            --model model \
             --max_length_generation 1024 \
             --tasks "multiple-$lang" \
             --temperature $temperature \
             --n_samples 50 \
             --precision "fp16" \
             --allow_code_execution \
-            --continuous_batching_size 32 \
+            --continuous_batching_size 1 \
             --swap_space 128 \
             --save_references_path "/kaggle/working/$modelname/multipl-e/$lang/$temperature/references.json" \
             --save_generations_path "/kaggle/working/$modelname/multipl-e/$lang/$temperature/generations.json" \
