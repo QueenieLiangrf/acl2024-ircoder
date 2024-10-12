28d127825e3d811ce169c1b781efe78fdfc79dff
QueenieLiangrf <98098460+QueenieLiangrf@users.noreply.github.com>
2024-10-12 10:32:28 +0800

    Update evaluation.py

--- Evaluation_Scripts/code-harness/bigcode_eval/tasks/custom_metrics/multiple_metrics/evaluation.py
+++ Evaluation_Scripts/code-harness/bigcode_eval/tasks/custom_metrics/multiple_metrics/evaluation.py
@@ -92,6 +92,6 @@ def evaluate_problem(
             test_results["results"].append(j)
             with open(test_results_path, "w+") as f:
                 f.write(json.dumps(test_results, indent=2))
-        print("j", j)
-        break
+            print("j", j)
+            break
     
